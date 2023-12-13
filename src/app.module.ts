bd8da14e4e80a08cd9eae577c1444828d32c6743
unknown <fgozdz01@gmail.com>
2023-12-13 16:35:36 +0100

    graphql initial setup with dummy schema and creation of all needed modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver } from '@nestjs/apollo';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { join } from 'path';
+import { FilmsModule } from './films/films.module';
+import { SpeciesModule } from './species/species.module';
+import { VehiclesModule } from './vehicles/vehicles.module';
+import { StarshipsModule } from './starships/starships.module';
+import { PlanetsModule } from './planets/planets.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      driver: ApolloDriver,
+      typePaths: ['./**/*.graphql'],
+      definitions: {
+        path: join(process.cwd(), 'src/graphql.ts'),
+      }
+    }),
+    FilmsModule,
+    SpeciesModule,
+    VehiclesModule,
+    StarshipsModule,
+    PlanetsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
