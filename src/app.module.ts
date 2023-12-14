ee0c5d8fd3d3053a59255e17ac91828d2f7d4be7
unknown <fgozdz01@gmail.com>
2023-12-14 22:29:57 +0100

    temporary turn off cache

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver } from '@nestjs/apollo';
-import { CacheModule } from '@nestjs/cache-manager';
-import type { RedisClientOptions } from 'redis';
-import { redisStore } from 'cache-manager-redis-yet';
+// import { CacheModule } from '@nestjs/cache-manager';
+// import type { RedisClientOptions } from 'redis';
+// import { redisStore } from 'cache-manager-redis-yet';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { join } from 'path';
@@ -12,6 +12,8 @@ import { SpeciesModule } from './species/species.module';
 import { VehiclesModule } from './vehicles/vehicles.module';
 import { StarshipsModule } from './starships/starships.module';
 import { PlanetsModule } from './planets/planets.module';
+import { SwapiWrapperModule } from './swapi-wrapper/swapi-wrapper.module';
+import { HttpExtensionModule } from './http-extension/http-extension.module';
 
 @Module({
   imports: [
@@ -22,17 +24,19 @@ import { PlanetsModule } from './planets/planets.module';
         path: join(process.cwd(), 'src/graphql.ts'),
       }
     }),
-    CacheModule.register<RedisClientOptions>({
-      store: redisStore,
-      isGlobal: true,
-      ttl: 60 * 60 * 24,
-      url: 'redis://cache:6379' // replace it later with env value
-    }),
+    // CacheModule.register<RedisClientOptions>({
+    //   store: redisStore,
+    //   isGlobal: true,
+    //   ttl: 60 * 60 * 24,
+    //   url: 'redis://cache:6379' // replace it later with env value
+    // }),
     FilmsModule,
     SpeciesModule,
     VehiclesModule,
     StarshipsModule,
-    PlanetsModule
+    PlanetsModule,
+    SwapiWrapperModule,
+    HttpExtensionModule
   ],
   controllers: [AppController],
   providers: [AppService],
