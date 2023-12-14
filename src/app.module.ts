d9810731230931cc0a4829ba6a9d6982e93ee7f9
unknown <fgozdz01@gmail.com>
2023-12-14 02:14:27 +0100

    create compose for redis cache and register cache module in nest application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver } from '@nestjs/apollo';
+import { CacheModule } from '@nestjs/cache-manager';
+import type { RedisClientOptions } from 'redis';
+import { redisStore } from 'cache-manager-redis-yet';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { join } from 'path';
@@ -18,6 +21,12 @@ import { PlanetsModule } from './planets/planets.module';
       definitions: {
         path: join(process.cwd(), 'src/graphql.ts'),
       }
+    }),
+    CacheModule.register<RedisClientOptions>({
+      store: redisStore,
+      isGlobal: true,
+      ttl: 60 * 60 * 24,
+      url: 'redis://cache:6379' // replace it later with env value
     }),
     FilmsModule,
     SpeciesModule,
