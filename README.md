79360f68ecf544cfeb764aced4982d0a338e49b6
unknown <fgozdz01@gmail.com>
2023-12-15 13:41:51 +0100

    update readme and test for swapi-wrapper to work with cache

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
+## to build app with docker run these commands in order
+
 docker build -t dev-and-deliver .
 
 docker-compose up -d
+
+
+## to acces documentation check this url after starting app
+
+http://localhost:3000/graphql
--- src/swapi-wrapper/swapi-wrapper.service.spec.ts
+++ src/swapi-wrapper/swapi-wrapper.service.spec.ts
@@ -1,6 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SwapiWrapperService } from './swapi-wrapper.service';
 import { HttpExtensionModule } from '../http-extension/http-extension.module';
+import { HttpExtensionService } from '../http-extension/http-extension.service';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+
+class HttpExtensionServiceMock {};
+
+const cacheManagerMock = {
+  get: jest.fn(),
+  set: jest.fn(),
+};
 
 describe('SwapiWrapperService', () => {
   let service: SwapiWrapperService;
@@ -8,7 +17,11 @@ describe('SwapiWrapperService', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [HttpExtensionModule],
-      providers: [SwapiWrapperService],
+      providers: [
+        SwapiWrapperService,
+        { provide: HttpExtensionService, useClass: HttpExtensionServiceMock },
+        { provide: CACHE_MANAGER, useValue: cacheManagerMock },
+      ],
       exports: [SwapiWrapperService]
     }).compile();
 
